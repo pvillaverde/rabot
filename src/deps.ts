5f1a1009d00238a9be1666f39ed9d652e0018f7a
pvillaverde <pvillaverde@qualigy.com>
2023-03-10 19:26:07 +0100

    ⬆️ DenoDB 1.4.0

--- src/deps.ts
+++ src/deps.ts
@@ -3,7 +3,7 @@
 export * from "https://deno.land/x/discordeno@17.0.1/mod.ts";
 export * from "https://deno.land/x/discordeno@17.0.1/plugins/mod.ts";
 /** DenoDB dependencies */
-export * from "https://deno.land/x/denodb@v1.2.0/mod.ts";
+export * from "https://deno.land/x/denodb@v1.4.0/mod.ts";
 
 export { readLines } from "https://deno.land/std@0.178.0/io/mod.ts";
 /** logger */
