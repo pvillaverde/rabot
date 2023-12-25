b702ecd5b724f0a47422aa390cd483c334ee3956
FedelloKirfed <fedellokirfed@proton.me>
2023-12-25 18:00:59 +0100

    🩺 Set Healthcheck to 2 hours

--- src/healthcheck.ts
+++ src/healthcheck.ts
@@ -6,7 +6,7 @@ for await (const line of readLines(healthcheckFile)) {
    if (!line.length) continue;
    const timestamp = new Date(line);
    // If there is no update on healthcheck file in the last 15 minutes, container is unhealthy.
-   if (now.diff(moment(timestamp), 'minutes') >= 15) {
+   if (now.diff(moment(timestamp), 'minutes') >= 120) {
       Deno.exit(1);
    }
 }
